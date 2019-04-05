e4780222c503ac1570d8b90df4bd79a5e895ac96
HimanshuRattan <35609063+HimanshuRattan@users.noreply.github.com>
2019-04-05 15:25:27 +0530

    hands on form

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,54 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
+import Button from '@material-ui/core/Button';
+//import { withStyles } from '@material-ui/core/styles';
+ import { TextField } from "@material-ui/core"
 
-import { CircularProgress } from "@material-ui/core"
-import TrumpImage from "../images/dtrump.png"
 import "../style/index.css"
+
+
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+  },
+  input: {
+    display: 'none',
+  },
+  extendedIcon: {
+    marginRight: theme.spacing.unit,
+  },
+});
+
 const IndexPage = () => (
-  <React.Fragment>
-    <div className="rotate-div">
-      <img className="rotate-infinite" src={TrumpImage} />
-      <br />
-      <br />
-      <CircularProgress color="primary" />
-    </div>
-  </React.Fragment>
+  <div className="index-form">
+  <TextField
+  id="standard-with-placeholder"
+  label="First Name"
+  placeholder="First Name"
+  margin="normal"
+/><br/>
+  <TextField
+  id="standard-with-placeholder"
+  label="Last Name"
+  placeholder="Last Name"
+  margin="normal"
+/>
+<br/>
+  <TextField
+  id="standard-with-placeholder"
+  label="Registration Number"
+  placeholder="Registration Number"
+  margin="normal"
+/><br/>
+<Button variant="outlined" size="medium" color="primary">
+          Medium
+        </Button>
+<Button size="medium" variant="contained" color="primary">
+        Submit
+      </Button>
+</div>
+
+
 )
 
 export default IndexPage
